db8471f4475ea87f6647d9513a103a7b813dbb44
Bino <binonguyen1090@gmail.com>
2022-03-13 23:34:39 -0500

    add Rating component and style

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Container, Row, Col, Card } from "react-bootstrap";
-
+import { Card } from "react-bootstrap";
+import Rating from './Rating';
 const Product = ({product}) => {
   return (
-    <Card className='my-3 p-3 rounded' key={product._id}>
+    <Card className='my-3 p-3 rounded'>
         <a href={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top' />
         </a>
@@ -18,9 +18,9 @@ const Product = ({product}) => {
           </a>
 
           <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} from {product.numReviews} reviews
-            </div>
+            <Rating value={product.rating}
+                     text={`${product.numReviews}  reviews`} 
+            />
           </Card.Text>
 
           <Card.Text as='h3'>${product.price}</Card.Text>
