971a60d3f0107642b0da377aecfcb30e4dfba200
Bino <binonguyen1090@gmail.com>
2022-03-14 21:36:07 -0500

    add Router and Productpage UI

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import products from '../products'
+import Product from '../components/Product'
+import {Row, Col} from 'react-bootstrap'
+const ProductScreen = () => {
+  return (
+    <>
+        <h1> Product Sreen</h1>        
+    </>
+  )
+}
+
+export default ProductScreen
