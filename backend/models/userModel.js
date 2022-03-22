a87a60452ee78779994d82e3e4e5376e6d4d6be3
Bino <binonguyen1090@gmail.com>
2022-03-21 22:08:01 -0500

    custom authentication middleware with jwt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs'
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -23,6 +23,10 @@ const userSchema = mongoose.Schema({
 },{
     timestamps: true
 })
+
+userSchema.methods.matchPassword = async function(enterPassword){
+    return await bcrypt.compare(enterPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
