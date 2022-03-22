d97ed49ed81c129c39459505f87a4172a4546f7e
Bino <binonguyen1090@gmail.com>
2022-03-22 00:23:27 -0500

    add header when user log in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import {Navbar, Nav, Container} from "react-bootstrap"
+import {Navbar, Nav, Container, NavDropdown} from "react-bootstrap"
 import {LinkContainer} from "react-router-bootstrap";
+import {useDispatch, useSelector} from 'react-redux'
+
+
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const userLogin = useSelector(state => state.userLogin)
+  const {loading, error, userInfo} = userLogin
+
+
+  const logoutHandler = () => {
+    console.log('Logout')
+  }
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -16,10 +27,21 @@ const Header = () => {
                 <LinkContainer to="/cart">
                   <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
                 </LinkContainer>
-                <LinkContainer to="/login">
-                  <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
-                </LinkContainer>
 
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>Log Out</NavDropdown.Item>
+
+                  </NavDropdown>
+                ): <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+              </LinkContainer>
+                }
+
+                
                 </Nav>
               </Navbar.Collapse>
             </Container> 
