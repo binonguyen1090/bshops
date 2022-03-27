4aa2cf1c8786b122b1d3b2d6ff35497c91ba159c
Bino <binonguyen1090@gmail.com>
2022-03-27 14:33:10 -0500

    fixing when logout and login with another user, profile and my order not clear

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,USER_REGISTER_FAIL, USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS, USER_LOGIN_FAIL,USER_LOGOUT, USER_DETAIL_REQUEST, USER_DETAIL_SUCCESS, USER_DETAIL_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL } from '../constants/userConstant'
+import {USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,USER_REGISTER_FAIL, USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS, USER_LOGIN_FAIL,USER_LOGOUT, USER_DETAIL_REQUEST, USER_DETAIL_SUCCESS, USER_DETAIL_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAIL_RESET } from '../constants/userConstant'
 export const userLoginReducer = (state={}, action) => {
   switch(action.type){
         case USER_LOGIN_REQUEST:
@@ -36,6 +36,8 @@ export const userRegisterReducer = (state={}, action) => {
               return {loading: false, user: action.payload}
           case USER_DETAIL_FAIL:
               return {loading: false, error: action.payload}
+          case USER_DETAIL_RESET:
+              return {user: {}}
           default:
               return state
     }
