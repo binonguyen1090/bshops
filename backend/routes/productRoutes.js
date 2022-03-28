6726bac00349d5bbf439d343ac48f8ebf99a4b4e
Bino <binonguyen1090@gmail.com>
2022-03-27 22:51:00 -0500

    add edit and create product backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProducts,getProductById, deleteProduct } from '../controllers/productController.js'
+import {createProduct, updateProduct ,getProducts,getProductById, deleteProduct } from '../controllers/productController.js'
 import {protect,admin} from '../middleware/authMiddleware.js'
 
 
@@ -7,7 +7,13 @@ import {protect,admin} from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct)
+router.route('/:id')
+    .get(getProductById)
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct)
+    
 
 export default router
