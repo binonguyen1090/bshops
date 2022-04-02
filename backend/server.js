30a2182e87e99141833af7f1294b9637961dbb54
Bino <binonguyen1090@gmail.com>
2022-04-02 12:08:06 -0500

    modify server file

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import path from 'path'
+
 import {notFound,errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -28,6 +29,7 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+
 app.get('/api/config/paypal', (req, res) => 
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
@@ -35,16 +37,18 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*', (req,res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
-}else{
-    app.get('/', (req,res) => {
-        res.send("API is running...")
-    })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
+
 
 // make upload to be static folder
 // const __dirname = path.resolve()
