2da769f829261976dee5cd53dd178fcb3e21171f
Bino <binonguyen1090@gmail.com>
2022-04-02 11:40:13 -0500

    set up for heroku

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
 {
-     name: 'Airpods Wireless Bluetooth Headphones',
+     name: 'Airpods Bluetooth Headphones',
      image: '/images/airpods.jpg',
      description:
        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
@@ -12,7 +12,7 @@ const products = [
      numReviews: 4,
 },
 {
-    name: 'iPhone 11 Pro 256GB Memory',
+    name: 'iPhone 11 Pro 256GB',
     image: '/images/phone.jpg',
     description:
     'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
@@ -36,7 +36,7 @@ const products = [
     numReviews: 3,
 },
 {
-    name: 'Sony Playstation 4 Pro White Version',
+    name: 'Sony Playstation 4 Pro White',
     image: '/images/playstation.jpg',
     description:
     'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -48,7 +48,7 @@ const products = [
     numReviews: 3,
 },
 {
-    name: 'Logitech G-Series Gaming Mouse',
+    name: 'Logitech Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
     'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
@@ -60,7 +60,7 @@ const products = [
     numReviews: 2,
 },
 {
-    name: 'Amazon Echo Dot 3rd Generation',
+    name: 'Amazon Echo Dot 3rd',
     image: '/images/alexa.jpg',
     description:
     'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
